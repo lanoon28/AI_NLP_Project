71f9c4f7f86dcf85cbffb501e580c9af783256a0
jaehong <jaehong2353@gmail.com>
2023-12-26 17:47:39 +0900

    newsScrap merge

--- newsScrap.py
+++ newsScrap.py
@@ -6,8 +6,10 @@ import time
 import pymysql
 import os
 from dotenv import load_dotenv
+from konlpy.tag import Okt
+import re
 
-class newsScrap:
+class newsScrap :
     
     headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"}
     
@@ -115,6 +117,36 @@ class newsScrap:
         cnn.commit()
         cur.close()
         cnn.close()
+
+    def keyWord(self):
+        keyword_article = ''.join(self.articles)
+
+        tokenizer = Okt()
+        raw_pos_tagged = tokenizer.pos(keyword_article, norm=True, stem=True)
+
+        del_list = ['하다', '있다', '되다', '이다', '돼다', '않다', '그렇다', '아니다', '이렇다', '그렇다', '어떻다']
+
+        word_cleaned = []
+        for word in raw_pos_tagged:
+            if not word[1] in ["Josa", "Eomi", "Punctuation", "Foreign"]: # Foreign == ”, “ 와 같이 제외되어야할 항목들
+                if (len(word[0]) != 1) and (word[0] not in del_list): # 한 글자로 이뤄진 단어들을 제외 & 원치 않는 단어들을 제외
+            # 숫자나 이메일 형식의 단어 제외
+                    if not re.match(r'^[0-9]*$', word[0]) and not re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', word[0]):
+                        word_cleaned.append(word[0])
+
+        word_dic = {}
+
+        for word in word_cleaned:
+            if word not in word_dic:
+                word_dic[word] = 1 # changed from "0" to "1"
+            else:
+                word_dic[word] += 1
+
+        sorted_word_dic = sorted(word_dic.items(), key=lambda x:x[1], reverse=True)
+
+        for word, count in sorted_word_dic[:50]:
+            print("{0}({1})".format(word, count), end=" ")
+
 
 # test = newsScrap('롯데백화점')
 # test.createNewsLinks()
