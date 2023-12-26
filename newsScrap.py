0b552ac7c27afe337dfcce9f1878b0325ec2d140
jaehong <jaehong2353@gmail.com>
2023-12-26 17:45:21 +0900

    2

--- newsScrap.py
+++ newsScrap.py
@@ -7,7 +7,7 @@ import pymysql
 import os
 from dotenv import load_dotenv
 
-class newsScrap :
+class newsScrap:
     
     headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"}
     
