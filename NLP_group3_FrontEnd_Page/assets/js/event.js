8aa6b98f1e2fb582f87f90abb51f4e26874da3ee
You Tea Kwan <35172690+YTK0201@users.noreply.github.com>
2024-01-03 11:45:28 +0900

    Add files via upload

--- NLP_group3_FrontEnd_Page/assets/js/event.js
+++ NLP_group3_FrontEnd_Page/assets/js/event.js
@@ -25,9 +25,9 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener('DOMContentLoaded', function() {
   const displayTextElement = document.getElementById('detail_enterName');
   const displayDetailElement = document.getElementById('detailInfoText');
+
   // 페이지 2가 로드될 때 로컬 스토리지에서 텍스트를 가져와서 화면에 표시
   const textReceived = localStorage.getItem('textToSend');
-
 
   if (textReceived) {
     displayTextElement.textContent = `: ${textReceived}`;
@@ -38,7 +38,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // API 호출
   // API 엔드포인트 URL
-  const apiUrl = 'http://127.0.0.1:5000/api/data';
+  const apiUrl = 'http://127.0.0.1:5000/news/hot';
+  const avg_esti = '';
+
   // RESTful API로 GET 요청 보내기
   console.log('fetch 요청 전송');
   console.log(location.origin);
@@ -48,14 +50,11 @@ document.addEventListener('DOMContentLoaded', function() {
         // 응답이 성공적인지 확인
         // response 매개변수는 HTTP 응답 객체
 
-        console.log('response 이벤트 작동 1');
-
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
         }
                 
-        console.log(`Text inside ${"response"}: ${response}`)
-        console.log('response 이벤트 작동 2');
+        // console.log(`Text inside ${"response"}: ${response}`)
 
         // JSON 형태로 변환하여 반환
         return response.json();
@@ -69,11 +68,11 @@ document.addEventListener('DOMContentLoaded', function() {
           if ('avg_esti' in data) {
             console.log('Value of key:', data['avg_esti']);
 
-            localStorage.removeItem('progress_bar');
-            console.log('LocalStorage 초기화');
+            localStorage.setItem('progress_bar', data['avg_esti']);            
+            localStorage.setItem('detail_info', data['detail_info']);
 
-            localStorage.setItem('progress_bar', data['avg_esti']);
             console.log('LocalStorage 저장');
+
 
           } else {
             console.log('Key not found in data.');
@@ -85,6 +84,14 @@ document.addEventListener('DOMContentLoaded', function() {
       });
 
 
+  // Detail Information Text
+  const textNewsDocs = localStorage.getItem('detail_info');
+
+  if (textNewsDocs) {
+    displayDetailElement.textContent = `${textNewsDocs}`;
+    // 로컬 스토리지의 데이터를 한 번 사용하고 나면 삭제 (선택사항)
+    localStorage.removeItem('detail_info');
+  }
 
   // if (textReceived) {
   //   // 해당 id 요소가 존재하면 그 요소의 텍스트를 출력
